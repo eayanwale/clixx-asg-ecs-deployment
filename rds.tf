86c53fffe10e47c986562a8ea40df4bcce2a99ad
Enoch Ayanwale <enochayanwale@outlook.com>
2026-07-14 01:39:50 -0400

    fix: give ECS stack its own RDS instance instead of sharing the ASG's DB

--- rds.tf
+++ rds.tf
@@ -1,5 +1,5 @@
 data "aws_db_snapshot" "latest_prod_snapshot" {
-  db_snapshot_identifier = "clixx-recent-snapshot"
+  db_snapshot_identifier = "clixx-working-snapshot"
   most_recent            = true
 }
 
@@ -8,6 +8,7 @@ resource "aws_db_subnet_group" "clixx" {
   subnet_ids = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
 }
 
+###### ASG/EC2 STACK DB CLUSTER
 resource "aws_db_instance" "clixx" {
   identifier             = "clixx-restored"
   instance_class         = "db.m5.large"
@@ -15,6 +16,19 @@ resource "aws_db_instance" "clixx" {
   db_subnet_group_name   = aws_db_subnet_group.clixx.name
   vpc_security_group_ids = [aws_security_group.db-sg.id]
   availability_zone      = "${var.AWS_REGION}a"
+  publicly_accessible    = false
+  multi_az               = false
+  skip_final_snapshot    = true
+}
+
+###### ECS DB CLUSTER
+resource "aws_db_instance" "clixx-ecs-db" {
+  identifier             = "clixx-restored-ecs"
+  instance_class         = "db.m5.large"
+  snapshot_identifier    = data.aws_db_snapshot.latest_prod_snapshot.id
+  db_subnet_group_name   = aws_db_subnet_group.clixx.name
+  vpc_security_group_ids = [aws_security_group.db-sg.id]
+  availability_zone      = "${var.AWS_REGION}b"
   publicly_accessible    = false
   multi_az               = false
   skip_final_snapshot    = true
