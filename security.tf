0e69127cd81a2410882f3e511b4f6ecb93cc4850
Enoch Ayanwale <enochayanwale@outlook.com>
2026-07-16 01:22:52 -0400

    fix: allow bastion host to reach the RDS security group
    DB security group only allowed ingress from the app SG, so wp-cli/
    mysql commands run from the bastion couldn't reach the database.

--- security.tf
+++ security.tf
@@ -123,10 +123,13 @@ resource "aws_security_group" "db-sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.clixx-sg.id]
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    security_groups = [
+      aws_security_group.clixx-sg.id,
+      aws_security_group.bastion-sg.id,
+    ]
   }
 
   egress {
